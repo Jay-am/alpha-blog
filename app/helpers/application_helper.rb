3b5a8295902960454560d875f9b901a80fd0cbfe
Julia Mackamul <julia.mackamul@gmail.com>
2020-10-14 16:16:20 +0200

    code changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)  
     size = options[:size]
-  gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-  image_tag(gravatar_url, alt: user.username)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username)
   end
 
 end
