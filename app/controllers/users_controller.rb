a0429b6badf5392c9b0d09843119bf931383b487
Julia Mackamul <julia.mackamul@gmail.com>
2020-10-16 10:46:51 +0200

    application helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
     @users = User.all
@@ -10,11 +10,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-  @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -34,12 +32,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
